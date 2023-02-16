ba5f6c4d1ea7c0f00bcbe57ba79972e10ec3a87b
Taylor Knibb <tknibb@smith.edu>
2023-02-15 22:57:33 -0500

    attempt 1

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,68 @@
-class Conversation {
+import java.util.*;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
+public class Conversation {
+
+  /**
+   * This method asks how many conversation rounds the user wants to have. The chatbot takes in a 
+   * user input and then the chatbot responds either with mirror words that makes the conversation relate to 
+   * the user's input or if there are no mirror words, then the chatbot responds with a canned response that is random.
+   * @param args there are multiple lists; the transcript list that the entire chatbot is added to and the 
+   * words list which holds all the phrases said by the user. There is also the replies list that holds the random canned
+   * responses from the computer.
+   */
+  public static void main(String[] args) {
+    List<String> transcript = new ArrayList<>();
+
+    System.out.print("How many conversation rounds would you like to have? ");
+    int rounds = 0;
+    Scanner input = new Scanner(System.in);
+    rounds = input.nextInt();
+
+    transcript.add("Hi there! What's on your mind?");
+    System.out.println("Hi there! What's on your mind?");
+
+    for (int i = 0; i < rounds; i++) {
+      Scanner response = new Scanner(System.in);
+      String phrase = response.nextLine();
+      transcript.add(phrase);
+      
+      String[] words = phrase.split(" ");
+      for (String word : words) {
+        int x = Arrays.asList(words).indexOf(word);
+        if (word.equals("I")){
+          words[x] = "you";}
+        if (word.equals("me") || (word.equals("Me"))){
+          words[x] = "you";}
+        if (word.equals("am")){
+          words[x] = "are";}
+        if (word.equals("you") || word.equals("You")){
+          words[x] = "I";}
+        if (word.equals("my") || word.equals("My")){
+          words[x] = "your";}
+        if (word.equals("your") || word.equals("Your")){
+          words[x] = "my";}
+        }
+      String finalPhrase = String.join(" " , words);
+      if (finalPhrase.equals(phrase)){
+        String[] replies = {"Cool!", "Interesting!", "Hmm...", "Tell me more about that"};
+        Random rand = new Random();
+        int index = rand.nextInt(replies.length);
+        transcript.add(replies[index]);
+        System.out.println(replies[index]);
+      }
+      else{
+        transcript.add(finalPhrase);
+        System.out.println(finalPhrase);
+      }
+      
+    }
+    System.out.println("See ya! \n");
+    transcript.add("See ya!");
+    System.out.println("TRANSCRIPT:");
+
+
+    for (int x = 0; x < transcript.size(); x++){
+      System.out.println(transcript.get(x));
+    }
+    }
   }
-}
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+        Julie and Kim
+
 - Which session(s) of TA / office hours did you attend?
+        sunday 1-3pm ta hours
+
 - What are your initial impressions of Java? 
+        I was first stumped by Java because of the different syntax it has compared to python. I would be thinking of how I would complete something but in python so now I have to research all the ways to do it but in Java which is a little more complicated since it is so literal. 
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+        Programming is a learning process that requires many trials and errors that I can't let discourage me. There are many differences between python and Java; the main difference being the syntax. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+        What works for me is working with other people to talk through your ideas since I think multiple brains are better than one. That's also my advice for someone taking this course in the future; do not stress too much since everybody is in the same boat and everyone is willing to help you through an issue. 
